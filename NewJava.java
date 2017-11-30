d7c51a2e24a2cda097e2983913e4b2cf00a0f8be
sufiyan <sufiyanclabs@gmail.com>
2017-11-30 10:18:33 -0500

    added a new java file

---
+++ NewJava.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class NewJava {
+
+	public static void main(String[] args) {
+		
+	 String [] word ={"hey","heez","how","heii"};
+	 
+	 
+	 for(String w :word){
+		 if(w.startsWith("he"))
+			 System.out.println(w +" starts with he");
+		 
+	 }
+		
+
+	}
+
+}
